bf15faa077cdcbcf660baa9dd4ebc20e60b7c99a
Robbie <rwdarrow4582@eagle.fgcu.edu>
2019-04-17 23:58:58 -0400

    added settings button and started navigation

--- app/src/main/java/com/rwdarrow/carfinder/MapsActivity.java
+++ app/src/main/java/com/rwdarrow/carfinder/MapsActivity.java
@@ -17,6 +17,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
